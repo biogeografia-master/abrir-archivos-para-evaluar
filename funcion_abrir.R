1669d9f3ef9abbac0e84446bab3484c480261ae2
Jose Ramon <joseramon@geografiafisica.org>
2019-09-01 10:48:03 -0400

    generación de lista de archivos actualizado

--- funcion_abrir.R
+++ funcion_abrir.R
@@ -1,4 +1,6 @@
 abrir <- function(nomarch = 'README.md'){
-  larch <- list.files(path = '..', pattern = nomarch, recursive = T)
+  larch <- list.files(path = '..', pattern = nomarch,
+                      recursive = T, full.names = T
+  )
   file.edit(larch)
 }
